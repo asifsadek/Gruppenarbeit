84b1f21da9a0f2e64760fd831e5f30a3ddac99d5
Josef Ziegler <ippeb24@gmail.com>
2013-04-04 16:56:43 +0200

    Added project I18n which contains the static class I18n

--- TranslationToolbox/src/translationtoolbox/TranslationDialog.java
+++ TranslationToolbox/src/translationtoolbox/TranslationDialog.java
@@ -48,6 +48,11 @@ public class TranslationDialog extends javax.swing.JFrame {
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
         jButton1.setText("OK");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -89,6 +94,10 @@ public class TranslationDialog extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
